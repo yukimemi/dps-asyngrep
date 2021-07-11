3a5d5c2dd730bb4f66227e87c33ba48974c4a008
yukimemi <yukimemi@gmail.com>
2021-07-11 10:32:27 +0900

    :zap: Use fn instead of denops object.

--- denops/asyngrep/main.ts
+++ denops/asyngrep/main.ts
@@ -1,4 +1,15 @@
-import { _, Denops, execute, flags, fs, io, path, toml, vars } from "./deps.ts";
+import {
+  _,
+  Denops,
+  execute,
+  flags,
+  fn,
+  fs,
+  io,
+  path,
+  toml,
+  vars,
+} from "./deps.ts";
 
 type Tool = {
   name: string;
@@ -22,8 +33,8 @@ export async function main(denops: Denops): Promise<void> {
   clog({ cfg });
 
   // User config.
-  const userToml = (await denops.call(
-    "expand",
+  const userToml = (await fn.expand(
+    denops,
     (await vars.g.get(
       denops,
       "asyngrep_cfg_path",
@@ -48,7 +59,7 @@ export async function main(denops: Denops): Promise<void> {
   // Set default name.
   const tools = cfg.tool as Tool[];
   const executable = tools.find(
-    async (x) => (await denops.call("executable", x.cmd)) as boolean,
+    async (x) => (await fn.executable(denops, x.cmd)) as boolean,
   );
   const def = tools.find((x) => x.name === "default") ?? executable;
   clog({ def });
@@ -56,69 +67,76 @@ export async function main(denops: Denops): Promise<void> {
   let p: Deno.Process;
 
   denops.dispatcher = {
+    // deno-lint-ignore require-await
     async asyngrep(...args: unknown[]): Promise<void> {
-      clog({ args });
-      const arg = args as string[];
-      const a = flags.parse(arg);
-      const pattern = a._.length > 0
-        ? a._.join(" ")
-        : await denops.call("input", "Search for pattern: ");
-      const tool = a.tool ? tools.find((x) => x.name === a.tool) : def;
-      clog({ pattern });
-      clog({ tool });
-      if (!tool) {
-        console.warn(`Grep tool is not found !`);
-        return;
-      }
-      const userArg = arg.filter(
-        (x) => ![...a._, `--tool=${tool.name}`].includes(x),
-      );
-      clog({ userArg });
+      (async () => {
+        try {
+          clog({ args });
+          const arg = args as string[];
+          const a = flags.parse(arg);
+          const pattern = a._.length > 0
+            ? a._.join(" ")
+            : await fn.input(denops, "Search for pattern: ");
+          const tool = a.tool ? tools.find((x) => x.name === a.tool) : def;
+          clog({ pattern });
+          clog({ tool });
+          if (!tool) {
+            console.warn(`Grep tool is not found !`);
+            return;
+          }
+          const userArg = arg.filter(
+            (x) => ![...a._, `--tool=${tool.name}`].includes(x),
+          );
+          clog({ userArg });
 
-      const toolArg = _.uniq([...tool.arg, ...userArg].filter((x) => x));
-      const cwd = (await denops.call("getcwd")) as string;
-      const cmd = [tool.cmd, ...toolArg, pattern, cwd] as string[];
-      clog(`pid: ${p?.pid}, rid: ${p?.rid}`);
-      try {
-        clog("close process");
-        p.close();
-      } catch (e) {
-        clog(e);
-      }
-      clog({ cmd, cwd });
-      p = Deno.run({
-        cmd,
-        cwd,
-        stdin: "null",
-        stdout: "piped",
-        stderr: "piped",
-      });
+          const toolArg = _.uniq([...tool.arg, ...userArg].filter((x) => x));
+          const cwd = (await denops.call("getcwd")) as string;
+          const cmd = [tool.cmd, ...toolArg, pattern, cwd] as string[];
+          clog(`pid: ${p?.pid}, rid: ${p?.rid}`);
+          try {
+            clog("close process");
+            p.close();
+          } catch (e) {
+            clog(e);
+          }
+          clog({ cmd, cwd });
+          p = Deno.run({
+            cmd,
+            cwd,
+            stdin: "null",
+            stdout: "piped",
+            stderr: "piped",
+          });
 
-      clog(`pid: ${p?.pid}, rid: ${p?.rid}`);
-      await denops.call("setqflist", [], "r");
-      await denops.call("setqflist", [], "a", {
-        title: `[Search results for ${pattern} on ${tool.cmd}]`,
-      });
-      await denops.cmd("botright copen");
+          clog(`pid: ${p?.pid}, rid: ${p?.rid}`);
+          await fn.setqflist(denops, [], "r");
+          await fn.setqflist(denops, [], "a", {
+            title: `[Search results for ${pattern} on ${tool.cmd}]`,
+          });
+          await denops.cmd("botright copen");
 
-      if (p.stdout === null) {
-        return;
-      }
-      for await (const line of io.readLines(p.stdout)) {
-        clog({ line });
-        await denops.call("setqflist", [], "a", { lines: [line] });
-      }
+          if (p.stdout === null) {
+            return;
+          }
+          for await (const line of io.readLines(p.stdout)) {
+            clog({ line });
+            await fn.setqflist(denops, [], "a", { lines: [line] });
+          }
 
-      const [status, stdoutArray, stderrArray] = await Promise.all([
-        p.status(),
-        p.output(),
-        p.stderrOutput(),
-      ]);
-      const stdout = new TextDecoder().decode(stdoutArray);
-      const stderr = new TextDecoder().decode(stderrArray);
-      p.close();
+          const [status, stdoutArray, stderrArray] = await Promise.all([
+            p.status(),
+            p.output(),
+            p.stderrOutput(),
+          ]);
+          const stdout = new TextDecoder().decode(stdoutArray);
+          const stderr = new TextDecoder().decode(stderrArray);
+          p.close();
 
-      clog({ status, stdout, stderr });
+          clog({ status, stdout, stderr });
+        } catch (e) {
+          console.log(e);
+        }
+      })();
     },
   };
 
